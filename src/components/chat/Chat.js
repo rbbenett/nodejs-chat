247fc7af59102d3aea44bc9271435b721437717b
Robert <rbbenett0614@gmail.com>
2021-03-23 18:31:29 -0400

    Added send message function

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,26 +1,39 @@
-import React, { useContext, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../UserContext';
+import { Link, useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 let socket;
-
 const Chat = () => {
-  const ENDPOINT = 'localhost:5000';
-  const { user, setUser } = useContext(UserContext)
-  let { room_id, room_name } = useParams();
-  useEffect(() => {
-    socket = io(ENDPOINT);
-    socket.emit('join', { name: user.name, room_id, user_id: user.id })
-  }, [])
-  return (
-    <div>
-      <div>{room_id} {room_name}</div>
-      <h1>Chat {JSON.stringify(user)}</h1>
-      <Link to={'/'}>
-        <button>Go to Home</button>
-      </Link>
-    </div>
-  )
+    const ENDPOINT = 'localhost:5000';
+
+    const { user, setUser } = useContext(UserContext);
+    let { room_id, room_name } = useParams();
+    const [message, setMessage] = useState('');
+    useEffect(() => {
+        socket = io(ENDPOINT);
+        socket.emit('join', { name: user.name, room_id, user_id: user.id })
+    }, [])
+    const sendMessage = event => {
+        event.preventDefault();
+        if (message) {
+            console.log(message)
+            socket.emit('sendMessage', message, room_id, () => setMessage(''))
+        }
+    }
+    return (
+        <div>
+            <div>{room_id} {room_name}</div>
+            <h1>Chat {JSON.stringify(user)}</h1>
+            <form action="" onSubmit={sendMessage}>
+                <input type="text"
+                    value={message}
+                    onChange={event => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+                <button>Send Message</button>
+            </form>
+        </div>
+    )
 }
 
 export default Chat
