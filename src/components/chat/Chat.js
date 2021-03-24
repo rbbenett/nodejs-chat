ed630ab1fce51b9c6f567726d6a66f69f726a1c9
Robert <rbbenett0614@gmail.com>
2021-03-24 13:27:34 -0400

    Clients can recieve messages from other clients

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -4,36 +4,47 @@ import { Link, useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 let socket;
 const Chat = () => {
-    const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'localhost:5000';
 
-    const { user, setUser } = useContext(UserContext);
-    let { room_id, room_name } = useParams();
-    const [message, setMessage] = useState('');
-    useEffect(() => {
-        socket = io(ENDPOINT);
-        socket.emit('join', { name: user.name, room_id, user_id: user.id })
-    }, [])
-    const sendMessage = event => {
-        event.preventDefault();
-        if (message) {
-            console.log(message)
-            socket.emit('sendMessage', message, room_id, () => setMessage(''))
-        }
+  const { user, setUser } = useContext(UserContext);
+  let { room_id, room_name } = useParams();
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit('join', { name: user.name, room_id, user_id: user.id })
+  }, [])
+
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
+
+  const sendMessage = event => {
+    event.preventDefault();
+    if (message) {
+      console.log(message)
+      socket.emit('sendMessage', message, room_id, () => setMessage(''))
     }
-    return (
-        <div>
-            <div>{room_id} {room_name}</div>
-            <h1>Chat {JSON.stringify(user)}</h1>
-            <form action="" onSubmit={sendMessage}>
-                <input type="text"
-                    value={message}
-                    onChange={event => setMessage(event.target.value)}
-                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-                />
-                <button>Send Message</button>
-            </form>
-        </div>
-    )
+  }
+
+  return (
+    <div>
+      <div>{room_id} {room_name}</div>
+      <h1>Chat {JSON.stringify(user)}</h1>
+      <pre>{JSON.stringify(messages, null, '\t')}</pre>
+      <form action="" onSubmit={sendMessage}>
+        <input type="text"
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+        />
+        <button>Send Message</button>
+      </form>
+    </div>
+  )
 }
 
 export default Chat
